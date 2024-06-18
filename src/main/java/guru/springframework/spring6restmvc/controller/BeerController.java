c6e44f7fc58d3c63bcfc689158abdfbbe369219f
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-06-18 15:21:33 +0200

    first commit

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -66,13 +66,12 @@ public class BeerController {
     }
 
 
-
     @GetMapping(value = BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(beerId);
+        return beerService.getBeerById(beerId).orElseThrow(NotFoundException::new);
     }
 
 }
